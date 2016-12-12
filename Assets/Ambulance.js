861a527148031e71ae3b3cf1c4d476c1cacb8aed
Simon Wixon <sw4g14@soton.ac.uk>
2016-12-12 22:59:48 +0000

    City scene ambulance and roads implemented

---
+++ Assets/Ambulance.js
@@ -0,0 +1,43 @@
+﻿#pragma strict
+
+private var rb:Rigidbody2D;
+var speed:float = 0.1;
+var maxspeed:float=1;
+
+function Start () {
+	rb = GetComponent(Rigidbody2D);
+}
+
+function FixedUpdate () {
+
+	
+
+	if(Input.GetKey(KeyCode.UpArrow)){
+		rb.AddForce(transform.up*speed);
+		rb.drag=5;
+	} else {
+		rb.drag=15;
+	}
+	if(Input.GetKey(KeyCode.DownArrow)){
+		rb.AddForce(-transform.up*speed*2);
+	}
+
+
+	var curspeed:Vector2 = Vector2(rb.velocity.x, rb.velocity.y);
+ 
+         if (curspeed.magnitude > maxspeed) {
+             curspeed = curspeed.normalized;
+             curspeed *= maxspeed;
+             rb.velocity=curspeed;
+         }
+
+         var forward:int = Input.GetKey(KeyCode.UpArrow)?1:-1;
+
+         if(Input.GetKey(KeyCode.LeftArrow)){
+		transform.Rotate(Vector3.forward * curspeed.magnitude*forward);
+	}
+	if(Input.GetKey(KeyCode.RightArrow)){
+		transform.Rotate(-Vector3.forward * curspeed.magnitude*forward);
+	}
+
+}
---
+++ Assets/FollowObject.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var target:Transform;
+
+function Start () {
+
+}
+
+function FixedUpdate () {
+	transform.position+=(target.position-transform.position)/8;
+	transform.position.z=-10;
+}
